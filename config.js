9522540b5b4cbe9a9983fe52b699b65cd4d651a7
jirayut-sa <122371918+jirayut-sa@users.noreply.github.com>
2026-07-21 18:42:24 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window.APP_CONFIG = {
+  // วาง URL ที่ลงท้ายด้วย /exec จาก Deploy > Web app
+  API_URL: 'https://script.google.com/macros/s/AKfycbxuRbBZMC0dMtJ9H26Uc7VAo7GpXLGgd0sa38YpXy1JOEVAqtl-2lA4SURIbQ58PR2Wjg/exec',
+  // ให้ตรงกับ APP_KEY ใน Script Properties (เว้นว่างได้ แต่ไม่แนะนำ)
+  APP_KEY: 'BKPD-claim-2026-x2050',
+  SCHOOL_NAME: 'โรงเรียนบ้านโคกประดู่',
+  SYNC_INTERVAL_MS: 5000
+};
